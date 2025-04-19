f70648039c450b70c4503a888d28fdf1db4d59ce
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-19 23:55:56 +0530

    add LinkedList class

--- src/LAB02/LinkedList.java
+++ src/LAB02/LinkedList.java
@@ -1,4 +1,18 @@
 package LAB02;
 
 public class LinkedList {
+    private int size;
+    private ChainNode head;
+
+    public LinkedList() {
+        this.size = 0;
+        this.head = null;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public int getSize() {
+        return size;
+    }
+
 }
